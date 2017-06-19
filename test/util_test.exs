038288d71d1bf58fff59de44e67de3b89a570937
Ellis Pritchard <ellis.pritchard@ft.com>
2017-06-19 18:57:41 +0100

    belated test around check_module_complies

---
+++ test/util_test.exs
@@ -0,0 +1,31 @@
+defmodule TestUtil do
+
+  use ExUnit.Case
+
+  def foo(_a,_b), do: 2
+
+  test "compliant module complies" do
+    assert __MODULE__ == Fettle.Util.check_module_complies(__MODULE__, __MODULE__, {:foo,2})
+  end
+
+  test "compliant module complies!" do
+    assert __MODULE__ == Fettle.Util.check_module_complies!(__MODULE__, __MODULE__, {:foo,2})
+  end
+
+  test "non-compliant module does not comply" do
+    assert {:error, _msg} = Fettle.Util.check_module_complies(__MODULE__, __MODULE__, {:foo,1})
+  end
+
+  test "non-compliant module does not comply!" do
+    assert_raise ArgumentError, fn -> Fettle.Util.check_module_complies!(__MODULE__, __MODULE__, {:foo,1}) end
+  end
+
+  test "non-extant module does not comply" do
+    assert {:error, _msg} = Fettle.Util.check_module_complies(__MODULE__.Bar, __MODULE__, {:foo,2})
+  end
+
+  test "non-extant module does not comply!" do
+    assert_raise ArgumentError, fn -> Fettle.Util.check_module_complies!(__MODULE__.Bar, __MODULE__, {:foo,2}) end
+  end
+
+end

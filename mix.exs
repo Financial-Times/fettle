6d05961c914aa04cefa7ac14d02fbe353e93831a
Ellis Pritchard <ellis.pritchard@ft.com>
2017-06-23 16:21:16 +0100

    add initial travis config

--- mix.exs
+++ mix.exs
@@ -62,7 +62,8 @@ defmodule Fettle.Mixfile do
       {:credo, "~> 0.5", only: [:dev, :test]},
       {:mix_test_watch, "~> 0.3", only: :dev, runtime: false},
       {:ex_doc, "~> 0.14", only: :dev, runtime: false},
-      {:dialyxir, "~> 0.5.0", only: [:dev], runtime: false}
+      {:dialyxir, "~> 0.5.0", only: [:dev], runtime: false},
+      {:inch_ex, ">= 0.0.0", only: :docs}
     ]
   end
 end
